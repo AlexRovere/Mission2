dde89df4c8c5e64cb7872737de2efca511c6f5c5
Quentin-BONNAFOUS <77445094+Quentin-BONNAFOUS@users.noreply.github.com>
2021-01-19 13:34:26 +0100

    CreationActualités

--- actualites.php
+++ actualites.php
@@ -3,13 +3,69 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="mainActualites.css">
     <title>Document</title>
 </head>
 <body>
 <?php include('header.php') ?>
 <?php include('menuSecondaire.php') ?>
 
+    <h2 id="titrePageActualites" >L’actualité d’Active Bretagne Informatique</h2>
+    <hr id="hrIndex">
+
+    <div class="blocImgActualites">
+
+        <div class="blocArticleActualites">
+            <img src="img/imgActualites1.png" alt="">
+            <h3>Nos solutions pour votre PME PMI</h3>
+            <p class="paragrapheActualites"> HO,HO,HO !!! Le tirage au sort du grand jeu de Noël ...</p>
+            <button class="enSavoirPlus">En savoir +</button>
+        </div>
+
+        <div class="blocArticleActualites">
+            <img src="img/imgActualites2.png" alt="">
+            <h3>Nos partenaires</h3>
+            <p class="paragrapheActualites"> HO,HO,HO !!! Le tirage au sort du grand jeu de Noël ...</p>
+            <button class="enSavoirPlus">En savoir +</button>
+        </div>
+
+        <div class="blocArticleActualites">
+            <img src="img/imgActualites3.png" alt="">
+            <h3>Jeu de Noël “ESN 2020”</h3>
+            <p class="paragrapheActualites"> HO,HO,HO !!! Le tirage au sort du grand jeu de Noël ...</p>
+            <button class="enSavoirPlus">En savoir +</button>
+        </div>
+
+    </div>
+
+    <div class="blocImgActualites">
+        
+        <div class="blocArticleActualites">
+            <img src="img/imgActualites1.png" alt="">
+            <h3>Nos solutions pour votre PME PMI</h3>
+            <p class="paragrapheActualites"> HO,HO,HO !!! Le tirage au sort du grand jeu de Noël ...</p>
+            <button class="enSavoirPlus">En savoir +</button>
+         </div>
+
+        <div class="blocArticleActualites">
+            <img src="img/imgActualites2.png" alt="">
+            <h3>Nos partenaires</h3>
+            <p class="paragrapheActualites"> HO,HO,HO !!! Le tirage au sort du grand jeu de Noël ...</p>
+            <button class="enSavoirPlus">En savoir +</button>
+        </div>
+
+        <div class="blocArticleActualites">
+            <img src="img/imgActualites3.png" alt="">
+            <h3>Jeu de Noël “ESN 2020”</h3>
+            <p class="paragrapheActualites"> HO,HO,HO !!! Le tirage au sort du grand jeu de Noël ...</p>
+            <button class="enSavoirPlus">En savoir +</button>
+        </div>
+
+    </div>
+
+    <button id="loadMore" name="button">LOAD MORE
+    <p id="textLoadMore"> HOLD SHIFT KEY TO LOAD ALL</p>
+</button>
 
 <?php include('footer.php') ?>
 </body>
