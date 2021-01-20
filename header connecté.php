7984b572ad01fcf9f877324d27bdcda1e74f75a2
198905 <66842416+198905@users.noreply.github.com>
2021-01-20 12:54:32 +0100

    header connecté css+ php

---
+++ header connecté.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS HEADER+CHAMPS DE CONNEXION.css">
+    <title>MisSion 2</title>
+</head>
+<body>
+
+    <header>
+        <div class="headerGauche">
+            <a href=""><img src="img/logo.jpg" id="logo" alt="1" height="120px"></a>
+            <div id="ACI">
+               <p class="active">active<br></p>
+               <p class="bretagne">bretagne<br></p>
+               <p class="informatique">informatique<br></p>
+            </div>
+        </div>
+        <div id="headerDroit">
+            <div class="identification">
+                <p>Bienvenue Jean-Pierre<br>
+                Vous etes connecté en tant que : Responsable Commercial</p>
+            </div>
+            <div class="boutons">
+                <div class="Intranet">
+                    <a href=""><p class="intranet">Intranet</p></a>
+                </div>
+                <div class="Deconnexion">
+                    <a href=""><p class="deconnexion">Deconnexion</p></a>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
