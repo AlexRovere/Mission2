c53c91bb7dea72cbb96e0598ec87b4230dfb5c4b
Laurence TURCOTTI <69862469+L-TCT@users.noreply.github.com>
2021-01-21 10:33:57 +0100

    jeudi 10h30
    dans main modif class tableau dans qui sommes nous et modif dans le fichier php
    fichier commercial envoyé

--- commercial.php
+++ commercial.php
@@ -4,42 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
-    <link rel="stylesheet" href="commercialUtilisateur.css">
-    <title>Gestion commercial utilisateur</title>
+    <link rel="stylesheet" href="commercial.css">
+    <title>Gestion commercial</title>
 </head>
 <body>
-<<<<<<< Updated upstream
 <?php include('headerConnecte.php') ?>
+<?php include('menuSecondaireIntranet.php') ?>
 <div class="miette"><a href="index.html"><img src="img/home.png" id="logoMiette"></a>  /Gestion Commerciale</div>
 <h2 class="h2Centre">GESTION COMMERCIALE</h2>
 <form class="searchForm">
     <input class="searchInput" type="search" value="" placeholder= "&#128269; Rechercher un client...">
-=======
-<?php include('header.php') ?>
-<div id="miette"><a href="index.html" id="lo"><img src="img/home.png" id="logoMiette"></a>  / exemple</div>
-<h2 class="h2Centre">GESTION COMMERCIALE</h2>
-<form class="searchForm">
-    <input class="searchInput" type="search" value="" placeholder="Rechercher un client...">
->>>>>>> Stashed changes
 </form>
 <div class="container">
     <aside class="sideBar">
             <ul class="function">
                 <li><a href="#">Liste des clients</a></li>
-                <li><a href="#">Contact client</a></li>
-<<<<<<< Updated upstream
                 <li><a href="#">Création client</a></li>
-=======
->>>>>>> Stashed changes
             </ul>
     </aside>
     <div class="interface">
+        <table class="tableauClient">
+            <thead class="titreTableauClient">
+                <tr>
+                    <th>ID</th>
+                    <th>RAISON SOCIALE</th>
+                    <th>TELEPHONE</th>
+                    <th><button class="tri">TRI</button></th>
+                </tr>
+            </thead>
+        </table>
     </div>
 </div>
 <?php include('footer.php') ?>
 </body>
-<<<<<<< Updated upstream
 </html>
-=======
-</html>
->>>>>>> Stashed changes
