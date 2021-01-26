e2ea5f1703d1eb3367b6a3db532ceb310f23f9f0
Laurence TURCOTTI <69862469+L-TCT@users.noreply.github.com>
2021-01-26 14:02:07 +0100

    creation dynamique

---
+++ dynamique/commercial.php
@@ -0,0 +1,42 @@
+<?php 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="main.css">
+    <title>Ative Bretagne Informatique</title>
+</head>
+<body>
+<?php include('headerConnecte.php') ?>
+<?php include('menuSecondaireIntranet.php') ?>
+<div class="miette"><a href="indexConnecte.php"><img src="img/home.png" id="logoMiette"></a><a href="mainIntranet.php">  / Accueil Intranet</a><a href="commercial.php">  / Gestion commerciale </a></div>
+<h2 class="h2Centre">Gestion commerciale</h2>
+
+<?php include('rechercheIntranet.php') ?>
+
+<div class="containerGestionCommercial">
+    <aside class="sideBar">
+        <ul class="function">
+            <li><a href="listeClient.php">Liste des clients</a></li>
+            <li><a href="creationClient.php">Création client</a></li>
+        </ul>
+    </aside>
+    <div class="interface">
+        <table class="tableauClient">
+            <thead class="titreTableauClient">
+                <tr>
+                    <th>ID</th>
+                    <th>RAISON SOCIALE</th>
+                    <th>TELEPHONE</th>
+                    <th><button class="tableauClientButton triButton">TRI</button></th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+<?php include('footer.php') ?>
+</body>
+</html>
---
+++ dynamique/script.js
@@ -0,0 +1,26 @@
+function showModal() {
+    const $modal = document.querySelector(".hiddenModal");
+    $modal.classList.replace("hiddenModal","modalComponent"); 
+    }
+
+function hideModal() {
+    const $modal = document.querySelector(".modalComponent");
+    $modal.classList.replace("modalComponent","hiddenModal"); 
+    }
+   
+    
+$verifConnexion = document.getElementById('formConnexion');
+$verifConnexion.addEventListener("submit", function(evenement)  {
+    $mail = document.getElementById('mailConnexion');
+    $mdp = document.getElementById('motDePasse');
+    if($mail.value == "") {
+        evenement.preventDefault();
+        alert("Tapez un email valide pour vous connecter");
+        document.$mail.focus(); 
+    }
+    else if ($mdp.value == "") {
+        evenement.preventDefault();
+        alert("Tapez un mot de passe valide pour vous connecter");
+        document.$mdp.focus(); 
+    }
+});
