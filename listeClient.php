c750775d31adb81fedeedf76dbfecefa94ada073
Quentin-BONNAFOUS <77445094+Quentin-BONNAFOUS@users.noreply.github.com>
2021-01-26 11:31:42 +0100

    ScriptAccesGestionComm

--- listeClient.php
+++ listeClient.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="main.css">
+    <script src="scriptAccesGestionCommerciale.js" async></script>
     <title>Ative Bretagne Informatique</title>
 </head>
 <body>
@@ -17,7 +18,7 @@
     <aside class="sideBar">
             <ul class="function">
                 <li><a href="listeClient.php">Liste des clients</a></li>
-                <li><a href="creationClient.php">Création client</a></li>
+                <li><a href="creationClient.php" class="accesActionClient">Création client</a></li>
             </ul>
     </aside>
     <div class="interface">
@@ -41,8 +42,8 @@
                             <li><a href="#">Fiche client</a></li>
                             <li><a href="#">Liste contact</a></li>
                             <li><a href="#">Liste documents</a></li>
-                            <li><a href="#">Modifier</a></li>
-                            <li><a href="#">Supprimer</a></li>
+                            <li><a href="#" class="accesActionClient">Modifier</a></li>
+                            <li><a href="#" class="accesActionClient">Supprimer</a></li>
                         </ul>
                     </td>
                 </tr>
@@ -56,8 +57,8 @@
                             <li><a href="#">Fiche client</a></li>
                             <li><a href="#">Liste contact</a></li>
                             <li><a href="#">Liste documents</a></li>
-                            <li><a href="#">Modifier</a></li>
-                            <li><a href="#">Supprimer</a></li>
+                            <li><a href="#"class="accesActionClient">Modifier</a></li>
+                            <li><a href="#"class="accesActionClient">Supprimer</a></li>
                         </ul>
                     </td>
                 </tr>
@@ -71,8 +72,8 @@
                             <li><a href="#">Fiche client</a></li>
                             <li><a href="#">Liste contact</a></li>
                             <li><a href="#">Liste documents</a></li>
-                            <li><a href="#">Modifier</a></li>
-                            <li><a href="#">Supprimer</a></li>
+                            <li><a href="#"class="accesActionClient">Modifier</a></li>
+                            <li><a href="#"class="accesActionClient">Supprimer</a></li>
                         </ul>
                     </td>
                 </tr>
@@ -86,8 +87,8 @@
                             <li><a href="#">Fiche client</a></li>
                             <li><a href="#">Liste contact</a></li>
                             <li><a href="#">Liste documents</a></li>
-                            <li><a href="#">Modifier</a></li>
-                            <li><a href="#">Supprimer</a></li>
+                            <li><a href="#"class="accesActionClient">Modifier</a></li>
+                            <li><a href="#"class="accesActionClient">Supprimer</a></li>
                         </ul>
                     </td>
                 </tr>
@@ -101,8 +102,8 @@
                             <li><a href="#">Fiche client</a></li>
                             <li><a href="#">Liste contact</a></li>
                             <li><a href="#">Liste documents</a></li>
-                            <li><a href="#">Modifier</a></li>
-                            <li><a href="#">Supprimer</a></li>
+                            <li><a href="#"class="accesActionClient">Modifier</a></li>
+                            <li><a href="#"class="accesActionClient">Supprimer</a></li>
                         </ul>
                     </td>
                 </tr>
