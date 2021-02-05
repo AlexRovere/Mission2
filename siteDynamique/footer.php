c92d5470a110a385d63b489132ebd69fefe78727
Quentin-BONNAFOUS <77445094+Quentin-BONNAFOUS@users.noreply.github.com>
2021-02-05 10:11:57 +0100

    Modif_SiteStatique

--- siteDynamique/footer.php
+++ siteDynamique/footer.php
@@ -4,6 +4,6 @@
             <li><a href="quiSommesNous">Qui sommes nous ?</a></li>
             <li><a href="actualites">Actualités</a></li>
             <li><a href="contact">Nous contacter</a></li>
-            <li><a href="mentions">Mentions légales</a></li>
+            <li><a href="#">Mentions légales</a></li>
         </ul>
     </footer>
--- siteStatique/menuSecondaireIntranet.php
+++ siteStatique/menuSecondaireIntranet.php
@@ -1,8 +1,8 @@
     <nav id="navSecondaire">
         <ul id="navListe">
-            <li class ="navSecondaireBox"><a href="ressourcesHumaines.php">Ressources Humaines</a></li>
+            <li class ="navSecondaireBox"><a href="#">Ressources Humaines</a></li>
             <li class ="navSecondaireBox"><a href="commercial.php">Commerciale</a></li>
-            <li class ="navSecondaireBox"><a href="projets.php">Projets</a></li>
+            <li class ="navSecondaireBox"><a href="#">Projets</a></li>
         </ul>
     </nav>
 
