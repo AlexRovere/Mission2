8ce92a1f4b75e3929a69716a6b20e5252727f1c7
AlexRovere <75780598+AlexRovere@users.noreply.github.com>
2021-10-10 11:59:16 +0200

    modif connexion bdd

--- siteDynamique/connexionBDD.php
+++ siteDynamique/connexionBDD.php
@@ -1,10 +1,7 @@
 <?php
 
-                try
-                {
-                    $bdd = new PDO('mysql:host=localhost;dbname=abi;charset=utf8', 'root', '');
-                }
-                catch (Exception $e)
-                {
-                    die('Erreur : ' . $e->getMessage());
-                }
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=agenaayh_abi;charset=utf8', 'agenaayh_alex', 'Subar91000&');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
--- siteDynamique/verifConnexion.php
+++ siteDynamique/verifConnexion.php
@@ -1,59 +1,52 @@
 <?php
 
 $post = file_get_contents('php://input'); //recupère les données
-if(!empty($post)){
+if (!empty($post)) {
     $data = json_decode($post, true); //décode le json
     $matricule = $data["matricule"];
     $mdp = $data["motDePasse"];
 
 
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=abi;charset=utf8', 'root', '', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-     ]);
-    
-}
-catch (Exception $e)
-{
-    die('Erreur : ' . $e->getMessage());
-}
-
-$req = $bdd->prepare('SELECT matricule, mdp, prenomCollabo, nomCollabo, qualification FROM collaborateur WHERE matricule = :matricule');
-$req->execute(array(
-    'matricule' => $matricule));
-$resultat = $req->fetch();
-
-if ($resultat == true) {
-$isPasswordCorrect = password_verify($mdp , $resultat['mdp']); //vérification avec hashage du mdp
-    if ($isPasswordCorrect) {
-        echo json_encode([ //encodage en json
-            "success" => true, 
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=agenaayh_abi;charset=utf8', 'agenaayh_alex', 'Subar91000&', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
-        session_start();
-        $_SESSION['matricule'] = $resultat['matricule'];
-        $_SESSION['prenom'] = $resultat['prenomCollabo'];
-        $_SESSION['nom'] = $resultat['nomCollabo'];
-        $_SESSION['qualification'] = $resultat['qualification'];
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
     }
-    else { //mdp incorrect
-        echo json_encode([
+
+    $req = $bdd->prepare('SELECT matricule, mdp, prenomCollabo, nomCollabo, qualification FROM collaborateur WHERE matricule = :matricule');
+    $req->execute(array(
+        'matricule' => $matricule
+    ));
+    $resultat = $req->fetch();
+
+    if ($resultat == true) {
+        $isPasswordCorrect = password_verify($mdp, $resultat['mdp']); //vérification avec hashage du mdp
+        if ($isPasswordCorrect) {
+            echo json_encode([ //encodage en json
+                "success" => true,
+            ]);
+            session_start();
+            $_SESSION['matricule'] = $resultat['matricule'];
+            $_SESSION['prenom'] = $resultat['prenomCollabo'];
+            $_SESSION['nom'] = $resultat['nomCollabo'];
+            $_SESSION['qualification'] = $resultat['qualification'];
+        } else { //mdp incorrect
+            echo json_encode([
+                "success" => false,
+                "errors" => [
+                    "Mauvais identifiant ou mot de passe ! "
+                ]
+            ]);
+        }
+    } else {
+        echo json_encode([ //identifiant incorrect
             "success" => false,
             "errors" => [
                 "Mauvais identifiant ou mot de passe ! "
             ]
         ]);
-    
-        }
-    }    
-else {
-    echo json_encode([ //identifiant incorrect
-        "success" => false,
-        "errors" => [
-            "Mauvais identifiant ou mot de passe ! "
-        ]
-    ]);
+    }
 }
-}
-?>
